f46ff119862508db17e2cbfa16b8986454a8237c
Josh Jenkins <clearharry@gmail.com>
2024-03-30 10:16:55 +1000

    add websocket realtime updates

--- include/OTAUpdater.h
+++ include/OTAUpdater.h
@@ -4,10 +4,17 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
 
+#include "WebSocketServer.h"
+
 class OTAUpdater {
  public:
+  OTAUpdater(WebSocketServer& webSocketServer)
+      : _webSocketServer(webSocketServer) {}
   void begin();
   void handle();
-  void performOTAUpdate(const String& updateUrl);
+  void performOTAUpdate();
   String checkForUpdate();
+
+ private:
+  WebSocketServer& _webSocketServer;
 };
