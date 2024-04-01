023bd515d5ec92759360975b4f6aaf7494b6be57
Josh Jenkins <clearharry@gmail.com>
2024-04-01 19:19:54 +1000

    improved OTA process

--- src/OTAUpdater.cpp
+++ src/OTAUpdater.cpp
@@ -1,85 +1,135 @@
 // OTAUpdater.cpp
 #include "OTAUpdater.h"
 
+#include <ArduinoJson.h>
 #include <ESP8266httpUpdate.h>
 
 #include "Settings.h"
 
 void OTAUpdater::begin() {
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_SPIFFS
-      type = "filesystem";
+  ArduinoOTA.onStart([this]() {
+    const char* commandType =
+        ArduinoOTA.getCommand() == U_FLASH ? "sketch" : "filesystem";
+    Serial.println("Start OTA updating " + String(commandType));
+
+    JsonDocument doc;
+    String type = "status";
+    doc["message"] = "Start OTA updating " + String(type);
+    _webSocketServer.broadcastMessage(type, doc);
+  });
+
+  ArduinoOTA.onEnd([this]() {
+    Serial.println("\nEnd");
+
+    JsonDocument doc;
+    String type = "status";
+    doc["message"] = "Update complete";
+    _webSocketServer.broadcastMessage(type, doc);
+  });
+
+  ArduinoOTA.onProgress([this](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA Progress: %u%%\r", (progress * 100) / total);
+
+    JsonDocument doc;
+    String type = "progress";
+    doc["percentage"] = (progress * 100) / total;
+    _webSocketServer.broadcastMessage(type, doc);
+  });
+
+  ArduinoOTA.onError([this](ota_error_t error) {
+    const char* errorMsg = "";
+    switch (error) {
+      case OTA_AUTH_ERROR:
+        errorMsg = "Auth Failed";
+        break;
+      case OTA_BEGIN_ERROR:
+        errorMsg = "Begin Failed";
+        break;
+      case OTA_CONNECT_ERROR:
+        errorMsg = "Connect Failed";
+        break;
+      case OTA_RECEIVE_ERROR:
+        errorMsg = "Receive Failed";
+        break;
+      case OTA_END_ERROR:
+        errorMsg = "End Failed";
+        break;
+      default:
+        errorMsg = "Unknown Error";
+        break;
     }
-    Serial.println("Start OTA updating " + type);
+    Serial.printf("OTA Error[%u]: %s\n", error, errorMsg);
+
+    JsonDocument doc;
+    String type = "error";
+    doc["message"] = String("OTA Error: ") + errorMsg;
+    _webSocketServer.broadcastMessage(type, doc);
   });
-  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("OTA Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR)
-      Serial.println("OTA Auth Failed");
-    else if (error == OTA_BEGIN_ERROR)
-      Serial.println("OTA Begin Failed");
-    else if (error == OTA_CONNECT_ERROR)
-      Serial.println("OTA Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR)
-      Serial.println("OTAReceive Failed");
-    else if (error == OTA_END_ERROR)
-      Serial.println("OTA End Failed");
-  });
+
   ArduinoOTA.begin();
 }
 
 void OTAUpdater::handle() { ArduinoOTA.handle(); }
 
-void OTAUpdater::performOTAUpdate(const String& updateUrl) {
+String OTAUpdater::checkForUpdate() {
+  HTTPClient http;
   WiFiClient client;
-  t_httpUpdate_return ret = ESPhttpUpdate.update(client, updateUrl);
+  http.begin(client, OTA_FIRMWARE_VERSION_URL);  // Defined in Settings.h
+  int httpCode = http.GET();
 
+  JsonDocument doc;
+  if (httpCode == HTTP_CODE_OK) {
+    String payload = http.getString();
+    http.end();
+
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error) {
+      Serial.println("Failed to parse JSON");
+      return "ERROR: Failed to parse JSON";
+    }
+
+    String newVersion = doc["version"].as<String>();
+    String changes = doc["changes"].as<String>();
+
+    Serial.println("Current firmware version: " + String(FIRMWARE_VERSION));
+    Serial.println("Available firmware version: " + newVersion);
+    if (newVersion != FIRMWARE_VERSION) {
+      return newVersion + "|" + changes;
+    }
+    return "NO_UPDATE";
+  } else {
+    Serial.printf("Failed to check for firmware updates, HTTP code: %d\n",
+                  httpCode);
+    http.end();
+    return "ERROR: HTTP request failed";
+  }
+}
+
+void OTAUpdater::performOTAUpdate() {
+  WiFiClient client;
+  auto ret = ESPhttpUpdate.update(client, OTA_FIRMWARE_URL);
+
+  String messageType;
+  JsonDocument doc;
   switch (ret) {
     case HTTP_UPDATE_FAILED:
       Serial.printf("HTTP_UPDATE_FAILED Error (%d): %s\n",
                     ESPhttpUpdate.getLastError(),
                     ESPhttpUpdate.getLastErrorString().c_str());
+      messageType = "error";
+      doc["message"] =
+          "HTTP_UPDATE_FAILED: " + ESPhttpUpdate.getLastErrorString();
       break;
     case HTTP_UPDATE_NO_UPDATES:
       Serial.println("HTTP_UPDATE_NO_UPDATES");
+      messageType = "status";
+      doc["message"] = "No updates available";
       break;
     case HTTP_UPDATE_OK:
       Serial.println("HTTP_UPDATE_OK");
+      messageType = "status";
+      doc["message"] = "Update successful";
       break;
   }
-}
-
-String OTAUpdater::checkForUpdate() {
-  WiFiClient client;
-  HTTPClient http;
-  http.begin(client,
-             "http://smoothcontrol.com/embedded-firmware/"
-             "flowmeter-pulse-scaler/version.txt");
-  int httpCode = http.GET();
-
-  if (httpCode == HTTP_CODE_OK) {
-    String newVersion = http.getString();  // Get the version string
-    newVersion.trim();  // Trim whitespace or newline characters
-    Serial.println("Current firmware version: " + String(FIRMWARE_VERSION));
-    Serial.println("Available firmware version: " + newVersion);
-
-    if (newVersion != FIRMWARE_VERSION) {
-      // The new version is different from the current version.
-      // The webserver manager should be alerted with the new version.a
-      return newVersion;
-    }
-  } else {
-    Serial.println("Failed to check for firmware updates");
-  }
-
-  http.end();
-  return "";  // Return an empty string if there's no update or an error
-              // occurred
+  _webSocketServer.broadcastMessage(messageType, doc);
 }
