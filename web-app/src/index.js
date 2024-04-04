48e9f4de2735b347cc29b14c8b48d81486322dee
Josh Jenkins <clearharry@gmail.com>
2024-04-04 15:38:22 +1000

    move firmware and about to multipage

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,17 +3,31 @@ import m from "mithril";
 
 import "../public/styles/bulma.min.css";
 import "../public/styles/bulma-cyborg-theme.css";
-import "../public/styles/styles.css";
+import "../public/styles/main.scss";
 
-import CalibrationPage from "./views/CalibrationPage";
-
-// Define routes
-const routes = {
-  "/": CalibrationPage
-};
+import MainLayout from "layouts/MainLayout";
+import CalibrationPage from "pages/calibration/CalibrationPage"
+import FirmwarePage from "components/pages/firmware/FirmwarePage";
+import AboutPage from "components/pages/AboutPage";
 
 // Arguments:
 // - document.body: root element to mount app to
 // - "/": default route path
 // - routes: route configuration object with keys as route paths and values as route components
-m.route(document.getElementById("app"), "/", routes);
+m.route(document.body, "/", {
+  "/": {
+    render: function () {
+      return m(MainLayout, m(CalibrationPage));
+    }
+  },
+  "/firmware": {
+    render: function () {
+      return m(MainLayout, m(FirmwarePage));
+    }
+  },
+  "/about": {
+    render: function () {
+      return m(MainLayout, m(AboutPage));
+    }
+  }
+});
