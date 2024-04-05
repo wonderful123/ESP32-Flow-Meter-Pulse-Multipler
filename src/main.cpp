c152e4db86efedfa5b2c96629bffc5ea7dfaab03
Josh Jenkins <clearharry@gmail.com>
2024-04-06 09:30:01 +1000

    add logging library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 // main.cpp
 #include "ApplicationManager.h"
+#include "Logger.h"
 
 ApplicationManager appManager;
 
-void setup() {
-  appManager.begin();
+// Foward declarations
+void serialLogCallback(Logger::Level level, const std::string& message);
+void initializeLogger();
+
+void setup() { appManager.begin(); }
+
+void loop() { appManager.loop(); }
+
+void initializeLogger() {
+  Serial.begin(115200);
+  while (!Serial);  // Wait for Serial port to connect
+
+  Logger::setLogCallback(serialLogCallback);
+  LOG_INFO("Logger initialized and ready");
+  LOG_INFO(R"(
+    ____  _       _ __        __   ____        __             _____            __         
+   / __ \(_)___ _(_) /_____ _/ /  / __ \__  __/ /_______     / ___/_________ _/ /__  _____
+  / / / / / __ `/ / __/ __ `/ /  / /_/ / / / / / ___/ _ \    \__ \/ ___/ __ `/ / _ \/ ___/
+ / /_/ / / /_/ / / /_/ /_/ / /  / ____/ /_/ / (__  )  __/   ___/ / /__/ /_/ / /  __/ /    
+/_____/_/\__, /_/\__/\__,_/_/  /_/    \__,_/_/____/\___/   /____/\___/\__,_/_/\___/_/     
+        /____/                                                                            
+  )");
 }
 
-void loop() {
-  appManager.loop();
+void serialLogCallback(Logger::Level level, const std::string& message) {
+  if (!Serial) return;
+  Serial.println(message.c_str());
+  Serial.flush();
 }
