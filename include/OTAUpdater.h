f2cf6f209acb548ec47ce2a9510242ff56e3f67d
Josh Jenkins <clearharry@gmail.com>
2024-04-11 12:05:53 +1000

    move to esp32fota lib

--- include/OTAUpdater.h
+++ include/OTAUpdater.h
@@ -3,22 +3,32 @@
 
 #include <Arduino.h>
 
+#define DISABLE_ALL_LIBRARY_WARNINGS  // Disable pragma debug messages from the
+                                      // esp32FOTA library
+#include <esp32FOTA.hpp>
+
 #include "WebSocketServer.h"
 
 class OTAUpdater {
  public:
-  OTAUpdater(WebSocketServer& webSocketServer)
-      : _webSocketServer(webSocketServer) {}
+  OTAUpdater(WebSocketServer& webSocketServer);
+
   void begin();
-  void handle();
   void performOTAUpdate();
   void checkForUpdate();
 
  private:
   WebSocketServer& _webSocketServer;
+  esp32FOTA _esp32FOTA;
 
-  void broadcastError(const String& message);
+  void logAndBroadcast(const String& type, const String& message);
   void broadcastUpdateAvailable(const String& newVersion,
                                 const String& changes);
-  void broadcastNoUpdate();
+  void parseManifest(const String& jsonPayload);
+
+  void setProgressCallback();
+  void setUpdateBeginFailCallback();
+  void setUpdateEndCallback();
+  void setUpdateCheckFailCallback();
+  void setUpdateFinishedCallback();
 };
