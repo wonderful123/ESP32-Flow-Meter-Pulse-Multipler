21d86f069ed6c09e2d8b55b39cc4b877731f4375
Josh Jenkins <clearharry@gmail.com>
2024-04-12 10:24:53 +1000

    Refactor WebSocketServer to support JSON data broadcasting and add string message broadcasting capability

--- include/WebSocketServer.h
+++ include/WebSocketServer.h
@@ -12,7 +12,8 @@ class WebSocketServer {
   explicit WebSocketServer(uint16_t port);
   void begin(AsyncWebServer* server);
   void broadcastPulseCount(unsigned long pulseCount);
-  void broadcastMessage(const String& type, const JsonVariant& data);
+  void broadcastJsonData(const String& type, const JsonVariant& data);
+  void broadcastMessage(const String& type, const String& message);
 
  private:
   AsyncWebSocket _webSocket;
