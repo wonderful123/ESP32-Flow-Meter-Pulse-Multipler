d7f3528ad05c69a5cd7b5be339e90ad40fb5f119
Josh Jenkins <clearharry@gmail.com>
2024-04-15 08:11:59 +1000

    remove unneeded code

--- include/FileSystemManager.h
+++ include/FileSystemManager.h
@@ -1,15 +1,8 @@
 // FileSystemManager.h
 #pragma once
 
-#include <Arduino.h>
-#include <ESPAsyncWebServer.h>
-#include <LittleFS.h>
-
 class FileSystemManager {
  public:
   FileSystemManager();
   bool mountFileSystem();
-  void serveStaticFiles(AsyncWebServer& server, const char* uri,
-                        const char* path,
-                        const char* defaultFile = "index.html");
 };
--- src/FileSystemManager.cpp
+++ src/FileSystemManager.cpp
@@ -1,19 +1,16 @@
 // FileSystemManager.cpp
 #include "FileSystemManager.h"
 
+#include <LittleFS.h>
+#include <Logger.h>
+
 FileSystemManager::FileSystemManager() {}
 
 bool FileSystemManager::mountFileSystem() {
   if (!LittleFS.begin()) {
-    Serial.println("[FileSystemManager] Error: Failed to mount LittleFS");
+    LOG_ERROR("Failed to mount LittleFS");
     return false;
   }
-  Serial.println("[FileSystemManager] LittleFS mounted successfully");
+  LOG_INFO("LittleFS mounted successfully");
   return true;
-}
-
-void FileSystemManager::serveStaticFiles(AsyncWebServer& server,
-                                         const char* uri, const char* path,
-                                         const char* defaultFile) {
-  server.serveStatic(uri, LittleFS, path).setDefaultFile(defaultFile);
 }
