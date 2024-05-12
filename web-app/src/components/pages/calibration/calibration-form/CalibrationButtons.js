8ad1f2370f07bb6329bc692608e94e3e9c88c489
Josh Jenkins <clearharry@gmail.com>
2024-05-13 07:57:10 +1000

    improve button state handling

--- web-app/src/components/pages/calibration/calibration-form/CalibrationButtons.js
+++ web-app/src/components/pages/calibration/calibration-form/CalibrationButtons.js
@@ -1,16 +1,33 @@
 // CalibrationButtons.js
 import m from "mithril";
+import IconCalibrationSpinner from 'icons/IconCalibrationSpinner';
 
 const CalibrationButtons = {
-  view: function () {
+  view: function (vnode) {
+    const {
+      onStart,
+      onStop,
+      startDisabled,
+      stopDisabled,
+      isCalibrationStarted
+    } = vnode.attrs;
+
     return m("div.buttons", [
       m("label.label", "Step 2: Run Calibration"),
       m("button.button.is-info.is-fullwidth", {
-        id: "start-calibration"
+        id: "start-calibration",
+        disabled: startDisabled,
+        onclick: onStart // Attach the onStart handler
       }, "Start Calibration"),
       m("button.button.is-danger.is-fullwidth", {
-        id: "stop-calibration"
-      }, "Stop Calibration")
+        id: "stop-calibration",
+        disabled: stopDisabled,
+        onclick: onStop // Attach the onStop handler
+      }, "Stop Calibration"),
+      isCalibrationStarted && m(IconCalibrationSpinner, {
+        class: "icon-class",
+        style: "width: 10px; height: auto;"
+      }),
     ]);
   }
 };
