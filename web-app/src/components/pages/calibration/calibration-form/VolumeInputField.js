9fce11622b98523d45f1f479f3adf95a36935be6
Josh Jenkins <clearharry@gmail.com>
2024-05-20 12:59:03 +1000

    Refactor VolumeInputField component for improved clarity and maintainability

--- web-app/src/components/pages/calibration/calibration-form/VolumeInputField.js
+++ web-app/src/components/pages/calibration/calibration-form/VolumeInputField.js
@@ -8,15 +8,7 @@ const MIN_VOLUME = 0.1;
 const VolumeInputField = {
   view: function (vnode) {
     const {
-      label,
-      units = 'L',
-      tooltipText,
-      id,
-      name,
-      placeholder,
-      value,
-      oninput,
-      ...inputAttributes
+      units = 'L', tooltipText, id, name, placeholder, value, oninput, ...inputAttributes
     } = vnode.attrs;
 
     const validateVolume = (volume) => {
@@ -31,38 +23,31 @@ const VolumeInputField = {
       }
     };
 
-    return m("div.field", [
-      m("label.label", {
-        for: id
-      }, [
-        label,
-        ' ',
-        m('span.unit', `(${units})`),
-        tooltipText && m(Tooltip, {
-          text: tooltipText
-        })
-      ]),
+    const displayValue = value === 0 ? '' : value;
+
+    return m("div.field.mb-5", [
       m("div.control.has-icons-left", [
         m("input.input[type=number]", {
           id,
           name,
-          step: MIN_VOLUME,
+          step: "0.1",
           min: MIN_VOLUME,
-          placeholder,
+          pattern: "^\\d+(\\.\\d+)?$",
+          placeholder: value === 0 ? placeholder : '',
           required: true,
-          value,
+          value: displayValue,
           oninput: oninput,
-          ...inputAttributes
+          ...inputAttributes,
         }),
         m("span.icon.is-left", [
           m(IconDrop, {
             class: "icon",
             style: "width: 10px; height: auto;"
           }),
-        ])
-      ])
+        ]),
+      ]),
     ]);
-  }
+  },
 };
 
 export default VolumeInputField;
