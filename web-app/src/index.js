8325a385c93a10716457a54593589561ad6d8d3d
Josh Jenkins <clearharry@gmail.com>
2024-05-25 09:33:30 +1000

    simply websocket connection

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -10,10 +10,8 @@ import AboutPage from "components/pages/AboutPage";
 
 import WebSocketService from "./services/WebSocketService";
 
-// Get the WebSocket port based on the environment
-const wsPort = process.env.WEBSOCKET_PORT;
-WebSocketService.connect(`ws://pulse-scaler.local:${wsPort}/ws`);
-// WebSocketService.connect('ws://' + window.location.hostname + ':80/ws');
+WebSocketService.connect();
+
 // Arguments:
 // - document.body: root element to mount app to
 // - "/": default route path
