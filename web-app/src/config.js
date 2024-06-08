32fcece3caf6976cb2f57ae337ca07c84fd8c278
Josh Jenkins <clearharry@gmail.com>
2024-06-09 07:04:18 +1000

    simplify config

--- web-app/src/config.js
+++ web-app/src/config.js
@@ -1,15 +1,17 @@
 // config.js
 const config = {
   API: {
-    baseURL: process.env.API_BASE_URL || 'http://pulse-scaler.local/api',
-    version: process.env.API_VERSION || '1',
+    baseURL: 'http://localhost',
+    port: 3000,
+    prefix: 'api',
+    version: 'v1',
   },
   websocket: {
-    url: process.env.WEBSOCKET_URL || 'ws://pulse-scaler.local',
-    port: process.env.WEBSOCKET_PORT || 80,
-    timeout: process.env.WEBSOCKET_TIMEOUT || 5000,
-    maxReconnectAttempts: process.env.WEBSOCKET_MAX_RECONNECT_ATTEMPTS || 10,
-    reconnectDelay: process.env.WEBSOCKET_RECONNECT_DELAY || 5000, // Add this property
+    url: 'ws://locahost',
+    port: 8085,
+    timeout: 5000,
+    maxReconnectAttempts: 10,
+    reconnectDelay: 5000,
   }
 };
 
