098bbcef8725f9848251d1e93fbce1380094db98
Josh Jenkins <clearharry@gmail.com>
2024-06-19 10:54:48 +1000

    initial commit

---
+++ web-app/src/services/ErrorHandler.js
@@ -0,0 +1,20 @@
+// ErrorHandler.js
+import m from "mithril";
+import ToastService from "./ToastService";
+
+const ErrorHandler = {
+  handleError: error => {
+    // Log the error to the console
+    console.error("Error:", error);
+
+    // Display an error toast notification
+    ToastService.error("An error occurred. Please try again later.");
+  },
+
+  handleSuccess: message => {
+    // Display a success toast notification
+    ToastService.success(message);
+  },
+};
+
+export default ErrorHandler;
