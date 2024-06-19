066dbeead53594c6ab4780e5d33737a3b773456d
Josh Jenkins <clearharry@gmail.com>
2024-06-19 10:52:53 +1000

    add toast test component

--- web-app/src/components/pages/test/TestPage.js
+++ web-app/src/components/pages/test/TestPage.js
@@ -1,20 +1,20 @@
 import m from "mithril";
 import SectionContainer from "components/common/SectionContainer";
-import NotificationCenter from "components/common/NotificationCenter";
 import Table from "./CalibrationRecords/Table";
 import Chart from "./CalibrationRecords/Chart";
-
+import ToastService from "../../../services/ToastService";
+import ErrorHandler from "../../../services/ErrorHandler";
 const TestPage = {
   view: () => {
     return m(SectionContainer, [
       m("h1.title", "Test Page"),
-      m(NotificationCenter, {}),
+      m("button", { onclick: () => ErrorHandler.handleSuccess("Calibration Successfully saved") }, "Test success toast"),
       m("hr"),
       m(Chart, {}),
       m("hr"),
       m(Table, {}),
     ]);
-  }
+  },
 };
 
 export default TestPage;
