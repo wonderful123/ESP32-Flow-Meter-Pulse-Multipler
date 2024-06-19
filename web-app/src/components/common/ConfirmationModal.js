74bc7172c6b062e1e4bd4cecf5c107f14ffbbc40
Josh Jenkins <clearharry@gmail.com>
2024-06-19 10:53:04 +1000

    fix escape key handling

--- web-app/src/components/common/ConfirmationModal.js
+++ web-app/src/components/common/ConfirmationModal.js
@@ -2,84 +2,39 @@
 import m from "mithril";
 
 const ConfirmationModal = {
-  onConfirm: null,
-  title: "",
-  message: "",
-  confirmText: "",
-  cancelText: "",
-
-  handleKeyDown: function (event) {
-    if (event.key === "Escape") {
-      this.props.onCancel();
-    }
-  },
-
-  handleClose: function (event) {
-    console.log('event', event);
-    this.props.onCancel();
-  },
-
   oncreate: function (vnode) {
-    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+    vnode.state.handleKeyDown = event => {
+      if (event.key === "Escape") {
+        vnode.attrs.onCancel();
+      }
+    };
+    window.addEventListener("keydown", vnode.state.handleKeyDown);
   },
 
   onremove: function (vnode) {
-    document.removeEventListener("keydown", this.handleKeyDown.bind(this));
+    // Remove the event listener from the window object when the modal is removed
+    window.removeEventListener("keydown", vnode.state.handleKeyDown);
   },
 
-  view: function (vnode) {
-    const {
-      isOpen,
-      onConfirm,
-      onCancel,
-      title,
-      message,
-      confirmText,
-      cancelText
-    } = vnode.attrs;
+  view(vnode) {
+    const { isOpen, onConfirm, onCancel, title, message, confirmText, cancelText } = vnode.attrs;
 
     if (!isOpen) return null;
 
-    const isActive = isOpen ? ".is-active" : "";
-
-    return m(`.modal${isActive}`, [
-      m(".modal-background", {
-        onclick: onCancel
-      }),
+    return m(".modal", { class: isOpen ? "is-active" : "" }, [
+      m(".modal-background", { onclick: onCancel }),
       m(".modal-card", [
         m("header.modal-card-head", [
           m("p.modal-card-title", title),
-          m("button.delete", {
-            "aria-label": "close",
-            onclick: onCancel
-          }),
+          m("button.delete", { "aria-label": "close", onclick: onCancel }),
         ]),
         m("section.modal-card-body", message),
-        m(
-          "footer.modal-card-foot",
-          m("div.buttons", [
-            m(
-              "button.button.is-danger", {
-                onclick: () => {
-                  onConfirm();
-                  onCancel();
-                },
-              },
-              confirmText
-            ),
-            m(
-              "button.button", {
-                onclick: onCancel
-              },
-              cancelText
-            ),
-          ])
-        ),
+        m("footer.modal-card-foot", [
+          m("button.button.is-danger", { onclick: onConfirm }, confirmText),
+          m("button.button", { onclick: onCancel }, cancelText),
+        ]),
       ]),
-      m("button.modal-close.is-large", {
-        "aria-label": "close",
-        onclick: onCancel
-      }),
+      m("button.modal-close.is-large", { "aria-label": "close", onclick: onCancel }),
     ]);
   },
 };
