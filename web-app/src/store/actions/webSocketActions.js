4dce837c002a5534c97b4ee840c0471c5a456e01
Josh Jenkins <clearharry@gmail.com>
2024-10-07 15:16:02 +1000

    simplify

--- web-app/src/store/actions/webSocketActions.js
+++ web-app/src/store/actions/webSocketActions.js
@@ -1,54 +1,25 @@
-// websocketActions.js
-export const WEBSOCKET_CONNECTED = 'WEBSOCKET_CONNECTED';
-export const WEBSOCKET_DISCONNECTED = 'WEBSOCKET_DISCONNECTED';
-export const WEBSOCKET_ERROR = 'WEBSOCKET_ERROR';
-export const WEBSOCKET_MESSAGE_RECEIVED = 'WEBSOCKET_MESSAGE_RECEIVED';
-export const WEBSOCKET_MESSAGE_SENT = 'WEBSOCKET_MESSAGE_SENT';
-export const WEBSOCKET_INVALID_MESSAGE = 'WEBSOCKET_INVALID_MESSAGE';
+// webSocketActions.js
 
-export const CALIBRATION_MESSAGE_RECEIVED = 'CALIBRATION_MESSAGE_RECEIVED';
-export const CONFIGURATION_MESSAGE_RECEIVED = 'CONFIGURATION_MESSAGE_RECEIVED';
-export const DIAGNOSTICS_MESSAGE_RECEIVED = 'DIAGNOSTICS_MESSAGE_RECEIVED';
+// Action Types
+export const WEBSOCKET_MESSAGE_RECEIVED = "WEBSOCKET_MESSAGE_RECEIVED";
+export const WEBSOCKET_CONNECTION_OPEN = "WEBSOCKET_CONNECTION_OPEN";
+export const WEBSOCKET_CONNECTION_CLOSE = "WEBSOCKET_CONNECTION_CLOSE";
+export const WEBSOCKET_CONNECTION_ERROR = "WEBSOCKET_CONNECTION_ERROR";
 
-export const websocketConnected = () => ({
-  type: WEBSOCKET_CONNECTED,
-});
-
-export const websocketDisconnected = () => ({
-  type: WEBSOCKET_DISCONNECTED,
-});
-
-export const websocketError = (error) => ({
-  type: WEBSOCKET_ERROR,
-  payload: error,
-});
-
-export const websocketMessageReceived = (message) => ({
+// Action Creators
+export const websocketMessageReceived = message => ({
   type: WEBSOCKET_MESSAGE_RECEIVED,
   payload: message,
 });
 
-export const websocketMessageSent = (message) => ({
-  type: WEBSOCKET_MESSAGE_SENT,
-  payload: message,
+export const websocketConnectionOpen = () => ({
+  type: WEBSOCKET_CONNECTION_OPEN,
 });
 
-export const calibrationMessageReceived = (calibrationData) => ({
-  type: CALIBRATION_MESSAGE_RECEIVED,
-  payload: calibrationData,
+export const websocketConnectionClose = () => ({
+  type: WEBSOCKET_CONNECTION_CLOSE,
 });
 
-export const configurationMessageReceived = (configurationData) => ({
-  type: CONFIGURATION_MESSAGE_RECEIVED,
-  payload: configurationData,
-});
-
-export const diagnosticsMessageReceived = (diagnosticsData) => ({
-  type: DIAGNOSTICS_MESSAGE_RECEIVED,
-  payload: diagnosticsData,
-});
-
-export const websocketInvalidMessage = (message) => ({
-  type: WEBSOCKET_INVALID_MESSAGE,
-  payload: message,
+export const websocketConnectionError = error => ({
+  type: WEBSOCKET_CONNECTION_ERROR,
 });
