69138031e52ebee3b0ec270da5c34325ac8d12fe
Josh Jenkins <clearharry@gmail.com>
2024-10-07 15:09:43 +1000

    add main dashboard

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -4,7 +4,8 @@ import m from "mithril";
 import "../public/styles/main.scss";
 
 import MainLayout from "layouts/MainLayout";
-import CalibrationPage from "pages/calibration/CalibrationPage"
+import DashboardPage from "pages/dashboard/DashboardPage";
+import CalibrationPage from "pages/calibration/CalibrationPage";
 import FirmwarePage from "components/pages/firmware/FirmwarePage";
 import AboutPage from "components/pages/AboutPage";
 import TestPage from "components/pages/test/TestPage";
@@ -20,22 +21,27 @@ WebSocketService.connect();
 m.route(document.body, "/", {
   "/": {
     render: function () {
+      return m(MainLayout, m(DashboardPage));
+    },
+  },
+  "/calibration": {
+    render: function () {
       return m(MainLayout, m(CalibrationPage));
-    }
+    },
   },
   "/firmware": {
     render: function () {
       return m(MainLayout, m(FirmwarePage));
-    }
+    },
   },
   "/about": {
     render: function () {
       return m(MainLayout, m(AboutPage));
-    }
+    },
   },
   "/test": {
     render: function () {
       return m(MainLayout, m(TestPage));
-    }
-  }
+    },
+  },
 });
