57e4ba4f56881546087f7617810f33caaa773b38
Josh Jenkins <clearharry@gmail.com>
2024-10-16 12:08:05 +1000

    remove unneeded espasync code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 // main.cpp
-#define _ESPASYNC_WIFIMGR_LOGLEVEL_ 1
-// #include <ESPAsync_WiFiManager.h>  // Declared here to avoid multiple linking errors specified in docs
 #include <HardwareSerial.h>
 
 #include "ApplicationManager.h"
