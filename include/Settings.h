bf0cdfa3eaa9907947aafcb6bb6a7e16b970791e
Josh Jenkins <clearharry@gmail.com>
2024-10-16 12:07:31 +1000

    add api and temp sensor pin

--- include/Settings.h
+++ include/Settings.h
@@ -1,9 +1,10 @@
 // Settings.h
 #pragma once
 
-#define PULSE_PIN 3
+#define PULSE_INPUT_PIN 3
 #define PULSE_BROADCAST_INTERVAL 250  // For websocket updates
 #define SCALED_OUTPUT_PIN 7
+#define TEMP_SENSOR_PIN 4
 
 #define BASE_PULSE_DURATION_MICROS \
   50000  // The generated scaled pulse is dynamic however this sets the base
@@ -15,6 +16,10 @@
 #define OTA_FIRMWARE_MANIFEST_URL "http://smoothcontrol.com/embedded-firmware/flowmeter-pulse-scaler/version.json"
 #define MDNS_DOMAIN_NAME "pulse-scaler"
 #define AP_SSID_NAME "Pulse Scaler"
+
+#define API_PORT 80
+#define API_PREFIX "/api"
+#define API_VERSION "v1"
 
 #define DEFAULT_CALIBRATION_FACTOR 1.0
 #define CARDINAL_SPLINE_TENSION_FACTOR 0.5
