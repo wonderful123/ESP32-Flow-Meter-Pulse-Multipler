f5d254d33105adb11528d4bb2fdb4ea84c33c471
Josh Jenkins <clearharry@gmail.com>
2024-10-30 11:31:07 +1000

    fix formatting

--- web-app/src/components/pages/calibration/calibration-form/VolumeInputField.js
+++ web-app/src/components/pages/calibration/calibration-form/VolumeInputField.js
@@ -1,29 +1,27 @@
 // VolumeInputField.js
 import m from "mithril";
 import IconDrop from "icons/IconDrop";
-import Tooltip from 'components/common/Tooltip';
+import Tooltip from "components/common/Tooltip";
 
 const MIN_VOLUME = 0.1;
 
 const VolumeInputField = {
   view: function (vnode) {
-    const {
-      units = 'L', tooltipText, id, name, placeholder, value, oninput, ...inputAttributes
-    } = vnode.attrs;
+    const { units = "L", tooltipText, id, name, placeholder, value, oninput, ...inputAttributes } = vnode.attrs;
 
-    const validateVolume = (volume) => {
+    const validateVolume = volume => {
       const parsedVolume = parseFloat(volume);
       return !isNaN(parsedVolume) && parsedVolume >= MIN_VOLUME;
     };
 
-    const handleInputChange = (event) => {
+    const handleInputChange = event => {
       const newVolume = event.target.value;
       if (validateVolume(newVolume)) {
         oninput(newVolume);
       }
     };
 
-    const displayValue = value === 0 ? '' : value;
+    const displayValue = value === 0 ? "" : value;
 
     return m("div.field.mb-5", [
       m("div.control.has-icons-left", [
@@ -33,17 +31,14 @@ const VolumeInputField = {
           step: "0.1",
           min: MIN_VOLUME,
           pattern: "^\\d+(\\.\\d+)?$",
-          placeholder: value === 0 ? placeholder : '',
+          placeholder: value === 0 ? placeholder : "",
           required: true,
           value: displayValue,
           oninput: handleInputChange,
           ...inputAttributes,
         }),
-        m("span.icon.is-left", [
-          m(IconDrop, {
-            class: "icon",
-            style: "width: 10px; height: auto;"
-          }),
+        m("span.icon.is-left.is-small", [
+          m(IconDrop),
         ]),
       ]),
     ]);
