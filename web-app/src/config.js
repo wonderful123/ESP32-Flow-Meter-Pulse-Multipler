3a2fc909e460fe52c69af2d0c441c21c0d8887bc
Josh Jenkins <clearharry@gmail.com>
2024-10-30 11:28:53 +1000

    add webpack variables

--- web-app/src/config.js
+++ web-app/src/config.js
@@ -1,18 +1,18 @@
 // config.js
 const config = {
   API: {
-    baseURL: 'http://localhost',
+    baseURL: API_BASE_URL, // From webpack using define plugin
     port: 80,
-    prefix: 'api',
-    version: 'v1',
+    prefix: "api",
+    version: "v1",
   },
   websocket: {
-    url: 'ws://localhost',
-    port: 80,
+    url: WEBSOCKET_URL,
+    port: WEBSOCKET_PORT,
     timeout: 5000,
     maxReconnectAttempts: 10,
     reconnectDelay: 5000,
-  }
+  },
 };
 
 export default config;
