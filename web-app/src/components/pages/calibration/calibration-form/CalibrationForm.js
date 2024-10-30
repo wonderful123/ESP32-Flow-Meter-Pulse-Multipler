5a0f8bc24fc10c260999342c11c79c5000d24294
Josh Jenkins <clearharry@gmail.com>
2024-10-30 11:30:51 +1000

    reimplement form

--- web-app/src/components/pages/calibration/calibration-form/CalibrationForm.js
+++ web-app/src/components/pages/calibration/calibration-form/CalibrationForm.js
@@ -1,75 +1,74 @@
-// // CalibrationForm.js
-// import m from 'mithril';
-// import Button from 'components/common/Button';
-// import VolumeInputField from './VolumeInputField';
-// import PulseMetrics from './PulseMetrics';
-// import CalibrationFormState from './CalibrationFormState';
-// import PulseCountService from 'services/PulseCountService';
+import m from "mithril";
+import SectionContainer from "components/common/SectionContainer";
+import CalibrationControl from "./CalibrationControl";
+import VolumeInputField from "./VolumeInputField";
+import CalibrationButton from "./CalibrationButton";
+import CalibrationFormLogic from "./CalibrationFormLogic";
+import PulseCountService from "services/PulseCountService";
+import ToastService from "services/ToastService"; // Import the ToastService
 
+function CalibrationForm(vnode) {
+  const { selectedMode } = vnode.attrs;
+  // Set selectedMode in logic
+  CalibrationFormLogic.setSelectedMode(selectedMode);
 
-// function CalibrationForm() {
-//   let pulseCount = PulseCountService.getPulseCountStream();
+  // Get the pulse count stream
+  const inputPulseCount = PulseCountService.getInputPulseCountStream();
+  // Automatically redraw the view when inputPulseCount changes
+  inputPulseCount.map(() => {
+    m.redraw();
+  });
 
-//   return {
-//     oninit: () => {
-//       PulseCountService.pulseCount.map(() => m.redraw());
-//     },
+  const handleSaveCalibration = async () => {
+    try {
+      const response = await CalibrationFormLogic.saveCalibration();
+      ToastService.success(response.message); // Show success toast
+      m.redraw(); // Redraw the view to reflect the reset form fields
+    } catch (error) {
+      ToastService.error(error.message); // Show error toast if the save fails
+    }
+  };
 
-//     view: () => {
-//       return [
-//         m("h2.title.is-spaced", "Calibration Process:"),
-//         m("h3.subtitle.is-4.has-text-weight-semibold", "Step 1: Enter Target Volume (L)"),
-//         m(VolumeInputField, {
-//           id: "target-volume",
-//           units: "Liters",
-//           tooltipText: "Enter the desired target volume for calibration.",
-//           name: "targetVolume",
-//           placeholder: "Enter target volume",
-//           value: CalibrationFormState.formState.targetVolume,
-//           oninput: (value) => CalibrationFormState.handleTargetVolumeChange(value),
-//         }),
-//         m("h3.subtitle.is-4.has-text-weight-bold", "Step 2: Run Calibration"),
-//         m(Button, {
-//           text: CalibrationFormState.formState.startButtonText,
-//           onclick: () => CalibrationFormState.handleStartClick(),
-//           enabled: CalibrationFormState.formState.startEnabled,
-//           classes: 'is-info'
-//         }),
-//         m(Button, {
-//           text: "Stop Calibration",
-//           onclick: () => CalibrationFormState.handleStopClick(),
-//           enabled: CalibrationFormState.formState.stopEnabled,
-//           classes: 'is-danger'
-//         }),
-//         m("h3.subtitle.is-4.has-text-weight-semibold", "Step 3: Enter Observed Volume (L)"),
-//         m(VolumeInputField, {
-//           id: "observed-volume",
-//           units: "Liters",
-//           tooltipText: "Enter the observed volume in liters.",
-//           name: "observedVolume",
-//           placeholder: "Enter observed volume",
-//           value: CalibrationFormState.formState.observedVolume,
-//           oninput: (value) => CalibrationFormState.handleObservedVolumeChange(value),
-//         }),
-//         m(PulseMetrics, {
-//           pulseCount: pulseCount(),
-//           pulsesPerLiter: CalibrationFormState.formState.pulsesPerLiter,
-//           calibrationFactor: CalibrationFormState.formState.calibrationFactor,
-//         }),
-//         m(Button, {
-//           text: CalibrationFormState.formState.saveButtonText,
-//           onclick: () => CalibrationFormState.handleSaveClick(),
-//           enabled: CalibrationFormState.formState.saveEnabled,
-//           classes: `is-success ${CalibrationFormState.formState.saveSuccess ? 'is-animated' : ''}`
-//         }),
-//         m(Button, {
-//           text: 'Clear Form',
-//           onclick: () => CalibrationFormState.resetForm(),
-//           classes: 'is-info'
-//         }),
-//       ];
-//     },
-//   }
-// };
+  return {
+    view: function () {
+      return m(SectionContainer, [
+        m("h3.subtitle.is-4.has-text-weight-semibold", "Step 1: Enter Target Volume (L)"),
+        m(VolumeInputField, {
+          id: "target-volume",
+          units: "Liters",
+          tooltipText: "Enter the desired target volume for calibration.",
+          name: "targetVolume",
+          placeholder: "Enter target volume",
+          value: CalibrationFormLogic.getTargetVolume(),
+          oninput: value => {
+            CalibrationFormLogic.setTargetVolume(value);
+          },
+        }),
+        m("h3.subtitle.is-4.has-text-weight-semibold", "Step 2: Run Calibration"),
+        m(CalibrationControl),
+        m("div.box", [m("span.title.is-6", "Input Pulse Count"), m("div.has-text-weight-semibold", inputPulseCount())]),
+        m("h3.subtitle.is-4.has-text-weight-semibold", "Step 3: Enter Observed Volume (L)"),
+        m(VolumeInputField, {
+          id: "observed-volume",
+          units: "Liters",
+          tooltipText: "Enter the observed volume.",
+          name: "observedVolume",
+          placeholder: "Enter observed volume",
+          value: CalibrationFormLogic.getObservedVolume(),
+          oninput: value => {
+            CalibrationFormLogic.setObservedVolume(value);
+          },
+        }),
+        m("h3.subtitle.is-4.has-text-weight-semibold", "Step 4: Save Calibration"),
+        m(CalibrationButton, {
+          className: "is-success",
+          label: "Save Calibration",
+          action: handleSaveCalibration,
+          isDisabled: !CalibrationFormLogic.isFormValid,
+        }),
+      ]);
+    },
+  };
+}
 
-// export default CalibrationForm;
+export default CalibrationForm;
