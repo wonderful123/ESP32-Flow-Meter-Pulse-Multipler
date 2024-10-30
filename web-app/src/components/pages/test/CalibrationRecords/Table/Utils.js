42e949cf3aef4aa29fb108ddc93c3a85d2483088
Josh Jenkins <clearharry@gmail.com>
2024-10-30 11:32:09 +1000

    unneeded

--- web-app/src/components/pages/test/CalibrationRecords/Table/Utils.js
+++
@@ -1,8 +0,0 @@
-// CalibrationRecords/Table/utils.js
-export const calibrationFactor = (targetOilVolume, observedOilVolume) => {
-  return (observedOilVolume / targetOilVolume * 100).toFixed(1) + '%';
-};
-
-export const pulsesPerLiter = (pulseCount, targetOilVolume) => {
-  return (pulseCount / targetOilVolume).toFixed(0);
-};
--- web-app/src/components/pages/test/CalibrationRecords/Table/index.js
+++
@@ -1,4 +0,0 @@
-// CalibrationRecords/Table/index.js
-import Table from './Table';
-
-export default Table;
